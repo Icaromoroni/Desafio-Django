c815e5d57b8891c4a980459e6049ccb59f4203e9
Icaro Moroni <icaromorone@gmail.com>
2024-05-19 00:06:10 -0300

    fix: permissão não estava funcionando, foi resolvido.

--- management/order_manager/permission.py
+++ management/order_manager/permission.py
@@ -6,9 +6,8 @@ class IsStaffUser(BasePermission):
     """
 
     def has_permission(self, request, view):
+    
+        if request.method in ['POST','PUT', 'PATCH', 'DELETE']:
+            if not request.user.is_staff:
+                return False
         return request.user and request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in ['PUT', 'PATCH', 'DELETE']:
-            return request.user.is_staff
-        return True
