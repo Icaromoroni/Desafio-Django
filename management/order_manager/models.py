57e21a188359ebcf466e09fbc9e31233243eb464
Icaro Moroni <icaromorone@gmail.com>
2024-05-19 15:18:53 -0300

    refactor/feat: implementação de metodos para atualizar o valor total ao salvar pedidos automaticamente.

--- management/order_manager/models.py
+++ management/order_manager/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 User = get_user_model()
 
@@ -13,10 +15,20 @@ class Item(models.Model):
 
 
 class Order(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    itens = models.ManyToManyField(Item)
-    data_pedido = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    itens = models.ManyToManyField(Item, related_name='orders')
+    order_date = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
-        return f'Order {self.id} - {self.usuario.username}'
+        return f'Order {self.id} - {self.user.username}'
+    
+    def att_total(self):
+        total = sum(item.price for item in self.itens.all())
+        self.total = total
+        self.save()
+
+@receiver(m2m_changed, sender=Order.itens.through)
+def att_total_order(sender, instance, **kwargs):
+    if kwargs['action'] in ['post_add', 'post_remove', 'post_clear']:
+        instance.att_total()
