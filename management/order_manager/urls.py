c88457aea8a33fc7b548ae49da1d2cb4db03839e
Icaro Moroni <icaromorone@gmail.com>
2024-05-19 21:00:07 -0300

    refactor: importação da views de order_manager compactada

--- management/order_manager/urls.py
+++ management/order_manager/urls.py
@@ -1,41 +1,22 @@
 from django.urls import path
-from .views import (
-    UserCreate,
-    UserDetailUpdate,
-    UserList,
-    ItemList,
-    ItemDetail,
-    ItemCreate,
-    ItemUpdate,
-    ItemDestroy,
-    OrderList,
-    OrderCreate,
-    OrderClienteDetail,
-    OrderEmployeeDetail
-    )
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from . import views
+
 
 urlpatterns = [
-    path('auth/users/', UserList.as_view()),
-    path('auth/user/add/', UserCreate.as_view()),
-    path('auth/user/<int:pk>/', UserDetailUpdate.as_view()),
-
-    path('itens/', ItemList.as_view()),
-    path('item/add/', ItemCreate.as_view()),
-    path('item/<int:pk>/', ItemDetail.as_view()),
-    path('item/up/<int:pk>/', ItemUpdate.as_view()),
-    path('item/del/<int:pk>/', ItemDestroy.as_view()),
-
-    path('orders/', OrderList.as_view()),
-    path('order/add/', OrderCreate.as_view()),
-    path('order/<int:pk>/', OrderClienteDetail.as_view()),
-    path('user/<int:pk>/order/', OrderList.as_view()),
-    path('user/<int:user_pk>/order/<int:order_pk>/', OrderEmployeeDetail.as_view()),
-
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Usuarios
+    path('auth/users/', views.UserList.as_view()),
+    path('auth/user/add/', views.UserCreate.as_view()),
+    path('auth/user/<int:pk>/', views.UserDetailUpdate.as_view()),
+    # Itens
+    path('itens/', views.ItemList.as_view()),
+    path('item/add/', views.ItemCreate.as_view()),
+    path('item/<int:pk>/', views.ItemDetail.as_view()),
+    path('item/up/<int:pk>/', views.ItemUpdate.as_view()),
+    path('item/del/<int:pk>/', views.ItemDestroy.as_view()),
+    # Orders
+    path('orders/', views.OrderList.as_view()),
+    path('order/add/', views.OrderCreate.as_view()),
+    path('order/<int:pk>/', views.OrderClienteDetail.as_view()),
+    path('orders/user/<int:pk>/', views.OrderList.as_view()),
+    path('user/<int:user_pk>/order/<int:order_pk>/', views.OrderEmployeeDetail.as_view()),
 ]
