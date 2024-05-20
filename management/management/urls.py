28e5f2b8aceaa46f8967e8d81bc5c60d26fbcaf1
Icaro Moroni <icaromorone@gmail.com>
2024-05-19 21:01:59 -0300

    feat: adicionado as urls OpenAPI do Swagger + urls de token.

--- management/management/urls.py
+++ management/management/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('order_manager.urls')),
+    # OpenAPI 3
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+        # Autenticação
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Api order manager
+    path('api/', include('order_manager.urls')),
 ]
