004d59c141cafe255d1f6f11afbafcb3cccf44b1
Icaro Moroni <icaromorone@gmail.com>
2024-05-20 22:24:56 -0300

    feat: add related name para o campo user do model Order.

--- management/order_manager/models.py
+++ management/order_manager/models.py
@@ -15,7 +15,7 @@ class Item(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'orders')
     itens = models.ManyToManyField(Item, related_name='orders')
     order_date = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
